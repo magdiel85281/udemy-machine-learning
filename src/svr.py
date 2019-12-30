3ba0ca06fc79d026a75853118ac0a6c6f976af3a
magdiel85281 <magdiel.delgadillo@gmail.com>
2019-12-30 16:47:57 -0700

    revert to pre-scaled values for plots; modify arrays

--- src/svr.py
+++ src/svr.py
@@ -4,46 +4,55 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2].values
+dataset = pd.read_csv('../data/Position_Salaries.csv')
+X = dataset.loc[:, 'Level'].values.reshape(-1, 1)
+y = dataset.loc[:, 'Salary'].values.reshape(-1, 1)
 
 # Splitting the dataset into the Training set and Test set
 """from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 sc_y = StandardScaler()
 X = sc_X.fit_transform(X)
 y = sc_y.fit_transform(y)
 
 # Fitting SVR to the dataset
-from sklearn.svm import SVR
-regressor = SVR(kernel = 'rbf')
+regressor = SVR(kernel='rbf')
 regressor.fit(X, y)
 
 # Predicting a new result
-y_pred = regressor.predict(6.5)
+y_pred = regressor.predict(sc_X.transform(np.array([[6.5]])))
 y_pred = sc_y.inverse_transform(y_pred)
+print(f'SVR Prediction: {y_pred}')
 
-# Visualising the SVR results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, regressor.predict(X), color = 'blue')
+# Visualising the SVR results, convert back to pre_scaled values
+fig = plt.figure(figsize=(16, 8))
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color='red')
+plt.plot(sc_X.inverse_transform(X),
+         sc_y.inverse_transform(regressor.predict(X)),
+         color='blue')
 plt.title('Truth or Bluff (SVR)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
-plt.show()
+# plt.show()
+plt.savefig('../img/svr.png')
 
 # Visualising the SVR results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.01) # choice of 0.01 instead of 0.1 step because the data is feature scaled
 X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+fig = plt.figure(figsize=(16, 8))
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color='red')
+plt.plot(sc_X.inverse_transform(X_grid),
+         sc_y.inverse_transform(regressor.predict(X_grid)),
+         color='blue')
 plt.title('Truth or Bluff (SVR)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
-plt.show()
+# plt.show()
+plt.savefig('../img/svr_smooth.png')

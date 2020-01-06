c350c457e0f2ca7b9b771b9f99aa2aab22d26c5a
magdiel85281 <magdiel.delgadillo@gmail.com>
2020-01-06 07:47:24 -0700

    include default params; imgs from run

--- src/svm.py
+++ src/svm.py
@@ -4,66 +4,103 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from matplotlib.colors import ListedColormap
+
 
 # Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
-y = dataset.iloc[:, 4].values
+dataset = pd.read_csv('../data/Social_Network_Ads.csv')
+X = dataset.loc[:, ['Age', 'EstimatedSalary']].values
+y = dataset.loc[:, 'Purchased'].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.25,
+                                                    random_state=0)
 
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+X_train = sc.fit_transform(X_train.astype(float))
+X_test = sc.transform(X_test.astype(float))
 
 # Fitting SVM to the Training set
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'linear', random_state = 0)
+classifier = SVC(C=1.0,
+                 kernel='linear',
+                 degree=3,
+                 gamma='scale',
+                 coef0=0.0,
+                 shrinking=True,
+                 probability=False,
+                 tol=0.001,
+                 cache_size=200,
+                 class_weight=None,
+                 verbose=False,
+                 max_iter=-1,
+                 decision_function_shape='ovr',
+                 random_state=0)
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+print(f'Confusion Matrix:\n{cm}')
 
 # Visualising the Training set results
-from matplotlib.colors import ListedColormap
+fig = plt.figure(figsize=(16, 8))
 X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1,
+                               stop=X_set[:, 0].max() + 1, step=0.01),
+                     np.arange(start=X_set[:, 1].min() - 1,
+                               stop=X_set[:, 1].max() + 1,
+                               step=0.01))
+plt.contourf(X1, X2,
+             classifier.predict(np.array([X1.ravel(),
+                                          X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75,
+             cmap=ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
+    plt.scatter(X_set[y_set == j, 0],
+                X_set[y_set == j, 1],
+                color=ListedColormap(('red', 'green'))(i),
+                label=j)
 plt.title('SVM (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
-plt.show()
+# plt.show()
+plt.savefig('../img/svm_train.png')
 
 # Visualising the Test set results
-from matplotlib.colors import ListedColormap
+fig = plt.figure(figsize=(16, 8))
 X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1,
+                               stop=X_set[:, 0].max() + 1,
+                               step=0.01),
+                     np.arange(start=X_set[:, 1].min() - 1,
+                               stop=X_set[:, 1].max() + 1,
+                               step=0.01))
+plt.contourf(X1, X2,
+             classifier.predict(np.array([X1.ravel(),
+                                          X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75,
+             cmap=ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
+    plt.scatter(X_set[y_set == j, 0],
+                X_set[y_set == j, 1],
+                color=ListedColormap(('red', 'green'))(i),
+                label=j)
 plt.title('SVM (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
-plt.show()
+# plt.show()
+plt.savefig('../img/svm_test.png')
